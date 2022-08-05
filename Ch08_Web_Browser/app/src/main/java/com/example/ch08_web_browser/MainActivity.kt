a6ec58d2b48084b61f9b0f3efbfa63238871c98c
MinKyeong031 <mingyg.kim@gmail.com>
2022-08-05 15:12:05 +0900

    Ch08 심플 웹 브라우저 - 마무리

--- Ch08_Web_Browser/app/src/main/java/com/example/ch08_web_browser/MainActivity.kt
+++ Ch08_Web_Browser/app/src/main/java/com/example/ch08_web_browser/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.example.ch08_web_browser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private val goHomeButton: Button by lazy {
-        findViewById<Button>(R.id.goHomeButton)
+    private val goHomeButton: ImageButton by lazy {
+        findViewById<ImageButton>(R.id.goHomeButton)
     }
 
-    private val addressBar: Button by lazy {
-        findViewById<Button>(R.id.addressBar)
+    private val addressBar: EditText by lazy {
+        findViewById<EditText>(R.id.addressBar)
     }
 
-    private val goBackButton: Button by lazy {
-        findViewById<Button>(R.id.goBackButton)
+    private val goBackButton: ImageButton by lazy {
+        findViewById<ImageButton>(R.id.goBackButton)
     }
 
-    private val goForwardButton: Button by lazy {
-        findViewById<Button>(R.id.goForwardButton)
+    private val goForwardButton: ImageButton by lazy {
+        findViewById<ImageButton>(R.id.goForwardButton)
+    }
+
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
     }
 
     private val webView: WebView by lazy {
         findViewById<WebView>(R.id.webView)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById<ContentLoadingProgressBar>(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +62,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             loadUrl(DEFAULT_URL)
         }
@@ -63,7 +75,13 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(v.text.toString())
+                val loadingUrl = v.text.toString()
+
+                if (URLUtil.isNetworkUrl(loadingUrl)) {
+                    webView.loadUrl(loadingUrl)
+                } else {
+                    webView.loadUrl("https://$loadingUrl")
+                }
             }
 
             return@setOnEditorActionListener false
@@ -75,6 +93,39 @@ class MainActivity : AppCompatActivity() {
 
         goForwardButton.setOnClickListener {
             webView.goForward()
+        }
+
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient() {
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+
+            progressBar.hide()
+
+            goBackButton.isEnabled = webView.canGoBack()
+            goForwardButton.isEnabled = webView.canGoForward()
+
+            addressBar.setText(url)
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
         }
     }
 
